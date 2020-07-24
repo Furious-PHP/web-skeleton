6929221563bf6a82786da1e6315d361c25ad4bab
RasulArslanov <rasularslanoov@gmail.com>
2020-07-24 15:20:16 +0500

    Fixed ORMFactory

--- src/Infrastructure/Framework/Cycle/ORMFactory.php
+++ src/Infrastructure/Framework/Cycle/ORMFactory.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Infrastructure\Framework\Cycle;
 
-use Domain\User\Entity\UserRepository;
 use Psr\Container\ContainerInterface;
-use Domain\User\Entity\User;
 use Cycle\ORM;
 use Spiral\Database;
 
@@ -17,9 +15,7 @@ final class ORMFactory
         $orm = new ORM\ORM(new ORM\Factory($container->get(Database\DatabaseManager::class)));
 
         $schemas = $container->get('config')['schemas'];
-        foreach ($schemas as $schema) {
-            $orm = $orm->withSchema(new ORM\Schema($schema));
-        }
+        $orm = $orm->withSchema(new ORM\Schema($schemas));
 
         return $orm;
     }
